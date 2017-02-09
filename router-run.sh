889dd1a6b5ba985e0b18920a238eff39d6465d64
Stela Gavrailova <Stela.Gavrailova@bulpros.com>
2017-02-10 01:01:47 +0200

    Update starting script.

--- router-run.sh
+++ router-run.sh
@@ -5,4 +5,4 @@ export app_prefix='double-travel'
 
 ENABLE_XDEBUG="-d xdebug.remote_autostart=1 -d xdebug.remote_enable=1"
 # if you have a problem with this port - try turning off your skype application
-php -S localhost:80 $ENABLE_XDEBUG -t public login.php
+php -S localhost:8888 $ENABLE_XDEBUG -t public
